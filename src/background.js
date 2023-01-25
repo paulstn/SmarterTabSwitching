31e344c7bd19950ef2613ef87e7123b85fc1e12c
Paul Sebastian <paul_sebastian@live.com>
2023-01-25 14:15:03 -0800

    currently has the ability to move tabs automatically, as boilerplate code

---
+++ src/background.js
@@ -0,0 +1,21 @@
+chrome.tabs.onActivated.addListener(moveToFirstPosition);
+
+async function moveToFirstPosition(activeInfo) {
+  try {
+    await chrome.tabs.move(activeInfo.tabId, {index: 0});
+    console.log("Success.");
+  } catch (error) {
+    if (error == "Error: Tabs cannot be edited right now (user may be dragging a tab).") {
+      setTimeout(() => moveToFirstPosition(activeInfo), 50);
+    } else {
+      console.error(error);
+    }
+  }
+}
+
+async function getCurrentTab() {
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
