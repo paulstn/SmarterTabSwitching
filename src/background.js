d8bc587e5b571e736824aaf893f74988258fffa0
Paul Sebastian <paul_sebastian@live.com>
2023-02-01 10:17:27 -0800

    Working command switch to specific tab feature

--- src/background.js
+++ src/background.js
@@ -1,35 +1,22 @@
 // chrome.tabs.onActivated.addListener(moveToFirstPosition);
 
-chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: {tabId: tab.id},
-    func: contentScriptFunc,
-    args: ['action'],
-  });
-});
-
-function contentScriptFunc(name) {
-  alert(`"${name}" executed`);
-}
-
-
-
-async function moveToFirstPosition(activeInfo) {
-  try {
-    await chrome.tabs.move(activeInfo.tabId, {index: 0});
-    console.log("Success.");
-  } catch (error) {
-    if (error == "Error: Tabs cannot be edited right now (user may be dragging a tab).") {
-      setTimeout(() => moveToFirstPosition(activeInfo), 50);
-    } else {
-      console.error(error);
-    }
+// Listen for keyboard shortcut
+chrome.commands.onCommand.addListener(function(command) {
+  // the 'switch-tab' command is defined in the manifest
+  if (command === "switch-tab") {
+      console.log("command triggered");
+      // the 'query' method essentially queries chrome.tabs for an entry
+      // the parameter here is saying 'give me all the tabs within the current window'
+      // the second argument is a function we provide, that has an argument (tabs) which
+      //   is received from the query and then the function later switches between tabs
+      chrome.tabs.query({currentWindow: true}, function(tabs) {
+          console.log(tabs);
+          console.log(tabs[0]);
+          // the update method grabs the tab it receives in the first parameter and
+          //   makes its 'active' and 'highlight' features true, which acts to switch to it
+          // 'active' lets the tab be computationally active
+          // 'highlighted' does the actual switching to the tab
+          chrome.tabs.update(tabs[0].id, {active: true, highlighted: true});
+      });
   }
-}
-
-async function getCurrentTab() {
-  let queryOptions = { active: true, lastFocusedWindow: true };
-  // `tab` will either be a `tabs.Tab` instance or `undefined`.
-  let [tab] = await chrome.tabs.query(queryOptions);
-  return tab;
-}
+});
