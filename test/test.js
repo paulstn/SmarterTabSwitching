57aa090e5589925975e5d52f12f9277994e99da3
Soren Dahl <sorendahl@comcast.net>
2023-02-02 13:32:25 -0800

    Tinkering with test files

---
+++ test/test.js
@@ -0,0 +1,24 @@
+'use strict'
+
+import puppeteer from 'puppeteer';
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://news.ycombinator.com', {
+      waitUntil: 'networkidle2',
+    });
+    
+    const three = await page.evaluate(() => {
+        return 1 + 2;
+    });
+
+    if (three !== 3) {
+        console.error("error");
+    } else {
+        console.log("success");
+    }
+    
+  
+    await browser.close();
+  })();
