aa1b4f9d204d6e12fb7de20215be3cad6bcb9bc3
Paul Sebastian <paul_sebastian@live.com>
2023-02-08 14:50:30 -0800

    Base feature Fixes (#30)
    Changed tab switching logic over to using tab ids instead of tab indices. Also had all state be stored in session data due to 'service workers' being discarded constantly, so that the extension would work all the time across a window's session.
    -Fixed issues with lossy data (inconsistencies)
    -Fixed issues with performance by removing needless querying

--- src/background.js
+++ src/background.js
@@ -1,37 +1,44 @@
-var currTabIndex = 0;
-var lastTabIndex = -1;
+// keep an mru cache for every window
+
+// mru cache, not in use
+var mruCache = [];
 
 // Listen for keyboard shortcut
 chrome.commands.onCommand.addListener(function(command) {
   // the 'switch-tab' command is defined in the manifest
   if (command === "switch-tab") {
       console.log("command triggered");
-      // the 'query' method essentially queries chrome.tabs for an entry
-      // the parameter here is saying 'give me all the tabs within the current window'
-      // the second argument is a function we provide, that has an argument (tabs) which
-      //   is received from the query and then the function later switches between tabs
-      chrome.tabs.query({currentWindow: true}, function(tabs) {
-          console.log(tabs);
-          // we have assertions here to check if the last tab index is invalid
-          console.assert((lastTabIndex >= 0), "The last tab index is less than 0");
-          console.assert((lastTabIndex < tabs.length), "The last tab index is more than the number of tabs");
-          console.log(tabs[lastTabIndex]);
-          console.log(`lastTabIndex listed: ${lastTabIndex}`)
-          // the update method grabs the tab it receives in the first parameter and
-          //   makes its 'active' and 'highlight' features true, which acts to switch to it
-          // 'active' lets the tab be computationally active
-          // 'highlighted' does the actual switching to the tab
-          chrome.tabs.update(tabs[lastTabIndex].id, {active: true, highlighted: true});
+
+      // we're looking within session data to grab the lastTabId
+      chrome.storage.session.get(["lastTabId"]).then((result) => {
+        console.log("Value currently is " + result.lastTabId);
+        // the update method grabs the tab it receives in the first parameter and
+        //   makes its 'active' and 'highlight' features true, which acts to switch to it
+        // 'active' lets the tab be computationally active
+        // 'highlighted' does the actual switching to the tab
+        chrome.tabs.update(result.lastTabId, {active: true, highlighted: true});
       });
+
   }
 });
 
 // Listen for every new tab
 chrome.tabs.onActivated.addListener(function(activeInfo) {
-  // console.log(activeInfo);
-  lastTabIndex = currTabIndex;
-  chrome.tabs.get(activeInfo.tabId, function (tab) {
-    currTabIndex = tab.index;
+  // need to keep session data stored. otherwise, data will get removed when the service worker
+  // gets shut down, which happens if another program is in focus within the device
+
+  // this gets the value of currTabId from session storage, and sets lastTabId to it
+  chrome.storage.session.get(["currTabId"]).then((result) => {    
+    chrome.storage.session.set({ "lastTabId": result.currTabId }).then(() => {
+      console.log("lastTabId is set to " + result.currTabId);
+    });
   });
-  console.log(`lastTabIndex: ${lastTabIndex}, currTabIndex: ${currTabIndex}`);
+  // this set the value of currTabId to the current tab's id
+  chrome.storage.session.set({ "currTabId": activeInfo.tabId }).then(() => {
+    console.log("currTabId is set to " + activeInfo.tabId);
+  });
+
+  // TODO: should we implement an inverted index to have quick removing?
+
+  // mruCache.push(activeInfo.tabId);
 });
