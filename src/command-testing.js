8190f1c5664fd8db2f61eeb98b8265f91017d990
Ritesh Boddipalli <53877313+riteshsb@users.noreply.github.com>
2023-03-02 13:59:19 -0800

    Ritesh tab previews (#41)
    * changing over to tab id - reducing many needless queries
    
    * Fixed consistency issues by implementing constant data upload to session storage
    
    * removed needless comments
    
    * modified tab switching commands to work off of keydown listener
    
    * popup within html files sorta working, need to work on placement
    
    * modified popup stuff to use internal element creators and not previews
    
    * revamped the command listening behavior to become more robust and accurate
    
    * disappear box after ctrl released in multiple tab switching
    
    * Removed redundant methods
    
    * tab switching functionality for multiple tab switching (integrated with previews) works
    
    * Image Preview code so far
    
    Co-authored-by: Ritesh Boddipalli <riteshsb@users.noreply.github.com>
    
    * Code for preview pic1
    
    ---------
    
    Co-authored-by: Paul Sebastian <paul_sebastian@live.com>
    Co-authored-by: Ritesh Boddipalli <riteshsb@users.noreply.github.com>
    Co-authored-by: riteshboddipalli <riteshsb@uw.edu>

--- src/command-testing.js
+++ src/command-testing.js
@@ -90,7 +90,23 @@ window.addEventListener('keyup', (event) => {
                 text = document.createTextNode("Switch tab num times: " + numSwitches);
                 // text.style["text-align"] = "center";
 
+                // const url = window.location.href;
+                // pic1RelPath = `${window.location.origin}/src/preview-pics/1.png`;
+                // const pic1Path = new URL(pic1RelPath, url).href;
+
+                // const absolutePath = __dirname + pic1RelPath;
+                var pic1 = document.createElement('img');
+                pic1.onload = function() {
+                    console.log('Image loaded:', this.src);
+                };
+                pic1.onerror = function() {
+                    console.error('Error loading image:', this.src);
+                };
+                pic1.src = `/src/preview-pics/sampleImage.png`;
+
                 text_div.appendChild(text);
+                text_div.appendChild(pic1);
+
                 popup.appendChild(text_div);
                 document.body.appendChild(popup);
             } else {
